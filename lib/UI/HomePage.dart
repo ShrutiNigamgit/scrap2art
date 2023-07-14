0bdce7bdc5c9ca5e6ffa8ac2f93e2b4977da9d60
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2023-07-15 00:10:38 +0530

    Bottom Navigation completed, and Home page is made

---
+++ lib/UI/HomePage.dart
@@ -0,0 +1,198 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:flutter/material.dart';
+
+import 'BuyPage.dart';
+import 'Music.dart';
+import 'Profile.dart';
+import 'SellPage.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  int _bottomNavIndex = 0;
+  final iconList = <IconData>[
+    Icons.home,
+    Icons.shopping_cart,
+    Icons.music_note_outlined,
+    Icons.person_4_outlined,
+  ];
+
+  List<Widget> pagesList = <Widget>[
+    HomePageContent(),
+    BuyPage(),
+    MusicPage(),
+    ProfilePage(),
+    SellPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          shape: const CircleBorder(eccentricity: 0.0),
+          child: const Icon(Icons.add),
+          backgroundColor: Color(0xff8E97FD),
+          onPressed: () {
+            setState(() {
+              _bottomNavIndex = 4;
+              print("Index is tapped $_bottomNavIndex");
+            });
+          },
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: AnimatedBottomNavigationBar(
+          height: 70,
+          notchMargin: 10,
+          splashColor: Color(0xff8E97FD),
+          activeColor: Color(0xff8E97FD),
+          icons: iconList,
+          iconSize: 30,
+          elevation: 50,
+          blurEffect: true,
+          activeIndex: _bottomNavIndex,
+          gapLocation: GapLocation.center,
+          notchSmoothness: NotchSmoothness.sharpEdge,
+          onTap: (index) => setState(() {
+            _bottomNavIndex = index;
+            // print("Index is tapped $_bottomNavIndex");
+          }),
+        ),
+        body: pagesList[_bottomNavIndex]);
+  }
+}
+
+class HomePageContent extends StatefulWidget {
+  const HomePageContent({super.key});
+
+  @override
+  State<HomePageContent> createState() => _HomePageContentState();
+}
+
+class _HomePageContentState extends State<HomePageContent> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10.0, 50, 10, 70),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 8),
+              child: Text(
+                "What brings you to Scrap2Art?",
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.w400),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8),
+              child: Text(
+                "Features",
+                style: TextStyle(fontSize: 30),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 2,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Color(0xff8E97FD),
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset("assets/WelcomeImage11.png"),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            "Sell Scrap",
+                            style: TextStyle(color: Colors.white, fontSize: 15),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                        decoration: BoxDecoration(
+                            color: Color(0xffFA6E5A),
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset("assets/WelcomeImage12.png"),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              "Buy Scrap",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 15),
+                            )
+                          ],
+                        )),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                        decoration: BoxDecoration(
+                            color: Color(0xffFEB18F),
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset("assets/WelcomeImage21.png"),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              "Sell your creativity",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 15),
+                            )
+                          ],
+                        )),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                        decoration: BoxDecoration(
+                            color: Color(0xffFFCF86),
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset("assets/WelcomeImage22.png"),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              "Find something UNIQUE",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 15),
+                            )
+                          ],
+                        )),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/UI/SellPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SellPage extends StatefulWidget {
+  const SellPage({super.key});
+
+  @override
+  State<SellPage> createState() => _SellPageState();
+}
+
+class _SellPageState extends State<SellPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("This is Sell page"),
+    );
+  }
+}
