31403b36fcd0c6df17d0bbec82335684707e8609
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2023-07-17 22:04:56 +0530

    firebase auth  tested, not completely integr

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:scrap2art/registrationPages/initial_Welcome_Screen.dart';
 import 'package:scrap2art/registrationPages/loginPage.dart';
 import 'package:scrap2art/registrationPages/signUpPage.dart';
 
-void main()
-{
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: 'initial_Welcome_Screen',
@@ -12,6 +14,5 @@ void main()
         'initial_Welcome_Screen': (context) => initialWelcomeScreen(),
         // 'signUpPage': (context) => signUpPage(),
         // 'loginPage': (context) => loginPage(),
-
       }));
 }
--- lib/registrationPages/initial_Welcome_Screen.dart
+++ lib/registrationPages/initial_Welcome_Screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:scrap2art/registrationPages/loginPage.dart';
 import 'package:scrap2art/registrationPages/signUpPage.dart';
@@ -13,68 +14,95 @@ class _initialWelcomeScreenState extends State<initialWelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children:<Widget> [
-            Text(
-              "Scrap2Art",
-              style: TextStyle(
-                color:Color(0xFF8E97FD),
-                fontSize: 30,
-                fontWeight: FontWeight.w700,
-              )
-          ),
-            SizedBox(height:20,),
+          children: <Widget>[
+            Text("Scrap2Art",
+                style: TextStyle(
+                  color: Color(0xFF8E97FD),
+                  fontSize: 30,
+                  fontWeight: FontWeight.w700,
+                )),
+            SizedBox(
+              height: 20,
+            ),
             Image.asset(
               "assets/Vector.png",
             ),
-            SizedBox(height:25,),
-          Text(
-            "We are what we do",
-            style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.w700,
-          ),
-      ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20.0, 20, 20.0, 20),
-            child: Text(
-              "Sell your scrap items, unlock your artistic potential,and support sustainable living with Scrap2Art",
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w400,
+            SizedBox(
+              height: 25,
             ),
-      ),
-          ),
+            Text(
+              "We are what we do",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20.0, 20, 20.0, 20),
+              child: Text(
+                "Sell your scrap items, unlock your artistic potential,and support sustainable living with Scrap2Art",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => signUpPage()));
+              },
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.6,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Color(0xFF7583CA),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Center(
+                    child: Text(
+                  'SIGN UP',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
+              ),
+            ),
+            SizedBox(height: 20),
+            GestureDetector(
+              onTap: () {
 
-        GestureDetector(
-          onTap:(){Navigator.push(context, MaterialPageRoute(builder:(context)=>signUpPage()));},
-          child: Container(
-            width: MediaQuery.of(context).size.width * 0.6,
-            height : 40,
-            decoration: BoxDecoration(
-              color: Color(0xFF7583CA),
-              borderRadius: BorderRadius.circular(30),
-            ),
-            child: Center(child: Text('SIGN UP',style :TextStyle(color:Colors.white,fontWeight :FontWeight.bold,),)),
-          ),
+                // this is just for testing the firebase autenticatio and it works
+                // FirebaseAuth.instance
+                //     .createUserWithEmailAndPassword(
+                //         email: "a@b.com", password: "Xerox123")
+                //     .then((value) {
+                //   print("new user created succesfully"); 
+                //   return Navigator.push(context,
+                //       MaterialPageRoute(builder: (context) => loginPage()));
+                // }).onError((error, stackTrace) =>
+                //         print("error,${error.toString()} "));
+
+
+                Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => loginPage()));
+              },
+              child: Text(
+                "ALREADY HAVE AN ACCOUNT? LOG IN",
+                style: TextStyle(
+                  color: Color(0xFFA1A4B2),
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            )
+          ],
         ),
-
-    SizedBox(height:20),
-    GestureDetector(
-      onTap: (){Navigator.push(context, MaterialPageRoute(builder:(context)=>loginPage()));},
-      child: Text(
-      "ALREADY HAVE AN ACCOUNT? LOG IN",
-      style: TextStyle(
-      color:Color(0xFFA1A4B2),
-      fontSize: 14,
-      fontWeight: FontWeight.w500,
-      ),
-      ),
-    )],),
-
       ),
     );
   }
